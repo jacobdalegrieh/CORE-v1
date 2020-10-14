bf5329100342673a821d1df1f2db6c58a9e3f0fa
jacobdalegrieh <54174993+jacobdalegrieh@users.noreply.github.com>
2020-10-14 21:48:44 +0000

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,34 +1,47 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const Web3 = require("web3");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require("dotenv").config();
+
 module.exports = {
-    networks: {
-        kovan: {
-            provider: function() {
-                new HDWalletProvider(MNEMONIC = "oval merit shop cancel unlock aerobic border survey series sniff cricket return",
-                    `https://kovan.infura.io/d1e42319efe449c5b2a63687b8708991`
-                )
-            },
-            network_id: 42,
-            gas: 4600000,
-        },
-        //     mainnet: {
-        //         provider: mainnetProvider,
-        //         network_id: 1,
-        //         gas: 9999999,
-        //         gasPrice: 100000000000
-        //     }
+  networks: {
+    development: {
+      protocol: "http",
+      host: "localhost",
+      port: 7545,
+      gas: 5000000,
+      gasPrice: 5e9,
+      network_id: "*",
     },
-    compilers: {
-        solc: {
-            version: "0.6.12",
-            docker: false,
-            settings: {
-                optimizer: {
-                    enabled: true,
-                    runs: 200
-                },
-                evmVersion: "byzantium"
-            }
-        }
-    }
+    kovan: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://kovan.infura.io/v3/${process.env.INFURA_APIKEY}`
+        ),
+      network_id: 42,
+      gas: 4600000,
+    },
+    mainnet: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://mainnet.infura.io/v3/${process.env.INFURA_APIKEY}`
+        ),
+      network_id: 1,
+      gas: 9999999,
+      gasPrice: 100000000000,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.6.12",
+      docker: false,
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+        evmVersion: "istanbul",
+      },
+    },
+  },
 };
